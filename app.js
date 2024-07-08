2532bf51bfaff056c3b6016edf3fa48069a745f0
Farhan Khan <mostlyfarhan@gmail.com>
2024-07-08 12:34:43 +0500

    refactored
    refactored each function into generic functions and cut short the extra code.

--- app.js
+++ app.js
@@ -1,171 +1,59 @@
-/* ************************************************** */
+const resetBtn = document.querySelector('#reset');
+const selectWinningScore = document.querySelector('#targetScore');
 
-/* 
-let maximum = parseInt(prompt("Enter a number range? "));
-let randomNum = Math.floor(Math.random() * maximum);
-
-
-let isFalse = true;
-
-while(isFalse){
-    let guess = parseInt(prompt("Guess the number:"));
-    if (guess == randomNum){
-        console.log(`Congratulations! You entered right. The number was ${randomNum}`);
-        isFalse = false;
-    }
-    else if (guess > randomNum){
-        console.log('Too High');
-    }
-    else if (guess < randomNum){
-        console.log('Too Low');
-    }
-    else {
-        console.log('Wrong! Try again.');
-    }
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('.p1Display')
 }
-*/
-/* ************************************************** */
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('.p2Display')
+}
 
-/* let isRunning = true;
-let list = [];
-
-while (isRunning) {
-    let input = prompt("What do you want to do?");
-
- 
-    if (input.toLowerCase() == "quit"){
-        isRunning = false;
-        console.log("Good Bye, see you agian");
-    }
-    else if (input.toLowerCase() == "new"){
-        let newTodo = prompt("Add a new todo item");
-        list.push(newTodo);
-        console.log(`${newTodo} added to list`);
-    }
-    else if (input.toLowerCase() == "list"){
-        let i = 0;
-        console.log(`*********************`);
-        for (let item of list) {
-            console.log(`${i}: ${item}`);
-            i++;
-        }
-        console.log(`*********************`);
-    }
-    else if (input.toLowerCase() == "delete"){
-        let index = prompt("Select the index you want to delete");
-        list.splice(index, 1);
-        console.log(`${index}: ${list[index]} is deleted`);
-    }
-    else {
-        console.log("Wrong entry, try again!");
-    }
-
-} */
-
-/* ************************************************** */
-/* random color maker */
-/* const btn = document.querySelector('#btn')
-const h1 = document.querySelector('h1')
-
-btn.addEventListener('click', () => {
-    const colorPicker = RandColor()
-    document.body.style.backgroundColor = colorPicker
-    h1.innerText = colorPicker
-})
-
-function RandColor() {
-    const r = Math.floor(Math.random() * 255)
-    const g = Math.floor(Math.random() * 255)
-    const b = Math.floor(Math.random() * 255)
-    return `rgb(${r}, ${g}, ${b})`
-} */
-
-/* ************************************************** */
-/* form submission and adding to the list */
-
-/* const form = document.querySelector("#form");
-const input = document.querySelector("#input");
-const catList = document.querySelector('#cats');
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const newLi = document.createElement("li");
-    newLi.innerText = input.value;
-    cats.append(newLi);
-    input.value = "";
-});
- */
-/* ************************************************** */
-/* input and change events */
-/* const form = document.querySelector('#form');
-const input = document.querySelector("#input");
-const h1 = document.querySelector('h1');
-
-form.addEventListener('input', function(e){
-    e.preventDefault();
-    h1.innerText = "Welcome,";
-    h1.innerText += input.value;
-})
-
-h1.innerText = "Welcome back again"; */
-
-/* ************************************************** */
-
-const p1Btn = document.querySelector('#p1Button');
-const p2Btn = document.querySelector('#p2Button');
-const p1Display = document.querySelector('.p1Display');
-const p2Display = document.querySelector('.p2Display');
-const reset = document.querySelector('#reset');
-const selectWinningScore = document.querySelector('#winningScore');
-
-
-
-let p1Score = 0;
-let p2Score = 0;
 let winningScore;
 let isGameOver = false;
 
-p1Btn.addEventListener("click", function(){
-    if (!isGameOver){
-        p1Score += 1;
-        if (p1Score === winningScore){
-            isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
-        }
-        p1Display.textContent = p1Score;
-    }
-    
-})
 
-p2Btn.addEventListener("click", function(){
+function updateScore(player, opponent){
     if (!isGameOver){
-        p2Score += 1;
-        if (p2Score === winningScore){
+        player.score += 1;
+        if (player.score === winningScore){
             isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
         }
-        p2Display.textContent = p2Score;
+        player.display.textContent = player.score;
     }
-    
-})
+}
+
+p1.button.addEventListener("click", function (){
+    updateScore(p1, p2);
+});
+
+p2.button.addEventListener("click", function(){
+    updateScore(p2, p1);
+});
 
 selectWinningScore.addEventListener('change',function(){
     winningScore = parseInt(this.value);
-    resetFunc();
+    reset();
 } )
 
-reset.addEventListener("click", resetFunc );
+resetBtn.addEventListener("click", reset);
 
-function resetFunc(){
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0; 
+function reset(){
     isGameOver = false;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    for (let p of [p1, p2]){
+        p.score = 0;
+        p.display.textContent = 0;
+       
+        p.display.classList.remove('has-text-success', 'has-text-danger');  
+        p.button.disabled = false;
+    }
 }
 
 
